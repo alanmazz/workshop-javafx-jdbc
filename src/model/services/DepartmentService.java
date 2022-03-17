fb6544a93fea64c501b3fcd1bc96a217ae873162
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-17 14:37:18 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -15,4 +15,13 @@ public class DepartmentService {
 		List<Department> list = new ArrayList<Department>();
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
